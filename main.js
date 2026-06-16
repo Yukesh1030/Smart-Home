9d6634ab1fd89d080d92c7a9f92a1509eb5e6a8b
Yukesh1030 <yukeshyuki18@gmail.com>
2026-06-16 12:57:37 +0530

    Updated things

--- main.js
+++ main.js
@@ -64,7 +64,7 @@ document.addEventListener('DOMContentLoaded', () => {
     const observerOptions = {
       root: null,
       rootMargin: '0px',
-      threshold: 0.15
+      threshold: 0.05
     };
     
     const observer = new IntersectionObserver((entries, observer) => {
